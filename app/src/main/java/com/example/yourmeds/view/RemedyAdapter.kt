ce5c10164c233533198fde86b9bc84810d1f6bfb
Jessica Alves <jessica@suasvendas.com>
2022-07-06 16:26:35 -0300

    Adapter

--- app/src/main/java/com/example/yourmeds/view/RemedyAdapter.kt
+++ app/src/main/java/com/example/yourmeds/view/RemedyAdapter.kt
@@ -1,4 +1,43 @@
 package com.example.yourmeds.view
 
-class RemedyAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.yourmeds.R
+import com.example.yourmeds.model.Remedy
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+class RemedyAdapter(var remedyes: MutableList<Remedy>) :
+    RecyclerView.Adapter<RemedyAdapter.RemedyHolder>() {
+
+    inner class RemedyHolder(view: View) : RecyclerView.ViewHolder(view){
+        var editTextNome : TextInputEditText
+        var editTextDose : TextInputEditText
+        var editTextCor : TextInputLayout
+        var editTextData : TextInputEditText
+        var editTextDias : TextInputEditText
+        var editTextRepetir : TextInputEditText
+
+        init {
+            editTextNome = view.findViewById(R.id.editTextNome)
+            editTextDose = view.findViewById(R.id.editTextDose)
+            editTextCor = view.findViewById(R.id.editTextCor)
+            editTextData = view.findViewById(R.id.editTextData)
+            editTextDias = view.findViewById(R.id.editTextDias)
+            editTextRepetir = view.findViewById(R.id.editTextRepetir)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RemedyHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RemedyHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
